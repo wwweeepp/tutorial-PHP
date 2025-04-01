42a3d9c182e1f7d3e48f48115ea0109d0f7b3a8b
wwweeepp <nguyenhongtuan160504@gmail.com>
2025-04-01 15:27:05 +0700

    updateupdate

--- Bai17/index.php
+++ Bai17/index.php
@@ -1,6 +1,4 @@
 <?php
-    session_start();
-
     class Vemaybay {
         public $tenchuyenbay;
         public $ngaybay;
@@ -14,6 +12,32 @@
     }
 
     class Hanhkhach extends Nguoi{
-        public $ve;
+        public $vemaybay;
+        public $soluongve;
+        public $tongtien;
     }
+
+    $ve = new Vemaybay();
+    $ve->tenchuyenbay = "VN123";
+    $ve->ngaybay = "2025-04-01";
+    $ve->giave = 1500000;
+
+    $hanhkhach = new Hanhkhach();
+    $hanhkhach->hoten = "Nguyen Van A";
+    $hanhkhach->gioitinh = "Nam";
+    $hanhkhach->tuoi = 30;
+    $hanhkhach->vemaybay = $ve;
+    $hanhkhach->soluongve = 2;
+    $hanhkhach->tongtien = $hanhkhach->soluongve * $ve->giave;
+
+    echo "Thông tin hành khách:<br>";
+    echo "Họ tên: " . $hanhkhach->hoten . "<br>";
+    echo "Giới tính: " . $hanhkhach->gioitinh . "<br>";
+    echo "Tuổi: " . $hanhkhach->tuoi . "<br>";
+    echo "Thông tin vé máy bay:<br>";
+    echo "Tên chuyến bay: " . $hanhkhach->vemaybay->tenchuyenbay . "<br>";
+    echo "Ngày bay: " . $hanhkhach->vemaybay->ngaybay . "<br>";
+    echo "Giá vé: " . number_format($hanhkhach->vemaybay->giave, 0, ',', '.') . " VND<br>";
+    echo "Số lượng vé: " . $hanhkhach->soluongve . "<br>";
+    echo "Tổng tiền: " . number_format($hanhkhach->tongtien, 0, ',', '.') . " VND<br>";
 ?>
